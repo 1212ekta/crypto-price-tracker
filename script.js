f5564b9b5a878da13a791f81f0403f52bed4b827
1212ekta <ektasaw14@gmail.com>
2025-03-31 11:45:33 +0530

    Add files via upload
    A Cryptocurrency Price Tracker that displays real-time market data for Bitcoin, Ethereum, and Dogecoin

---
+++ script.js
@@ -0,0 +1,13 @@
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd',
+    "method": "GET",
+    "headers": {}
+};
+
+$.ajax(settings).done(function(response) {
+    document.getElementById('bitcoin').innerHTML = response.bitcoin.usd;
+    document.getElementById('ethereum').innerHTML = response.ethereum.usd;
+    document.getElementById('dogecoin').innerHTML = response.dogecoin.usd;
+});
